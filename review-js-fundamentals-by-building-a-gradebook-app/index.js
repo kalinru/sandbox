a69c2362ed4fa91b5c54ec4ad5b27962e34aa375
kalinru <52463446+kalinru@users.noreply.github.com>
2024-07-16 15:56:07 +0500

    eview-js-fundamentals-by-building-a-gradebook-app/step-2 fix
    https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/review-js-fundamentals-by-building-a-gradebook-app/step-2

--- review-js-fundamentals-by-building-a-gradebook-app/index.js
+++ review-js-fundamentals-by-building-a-gradebook-app/index.js
@@ -8,20 +8,17 @@ console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
 function getGrade(score) {
   if (score === 100) {
     return "A++";
-  }
-  if (score >= 90) {
+  } else if (score >= 90) {
     return "A";
-  }
-  if (score >= 80) {
+  } else if (score >= 80) {
     return "B";
-  }
-  if (score >= 70) {
+  } else if (score >= 70) {
     return "C";
-  }
-  if (score >= 60) {
+  } else if (score >= 60) {
     return "D";
+  } else {
+    return "F";
   }
-  return "F";
 }
 
 console.log(getGrade(96));
